b4f1bc15a2a3c40a5e770161a4c281159dd2f866
yulianasep <yuliana.sepulveda@alternova.com>
2023-06-29 22:46:22 -0500

    feat: erro information

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 from CharacterService import CharacterService
 
-
 services = CharacterService()
 
-character_id_input = input("Ingrese el ID del personaje que desea consultar: ")
-character_id = int(character_id_input.strip())
-character = services.get_character_by_id(character_id)
-print("\nPersonaje:")
-print(character)
-
-
-characters_input = input("\nIngrese los IDs de los personajes separados por comas: ")
-characters_list = [int(character_id.strip()) for character_id in characters_input.split(",")]
-
-character_group = services.get_multiple_characters(characters_list)
-print("\nGrupo de personajes:")
-for character in character_group:
+try:
+    character_id_input = input("Ingrese el ID del personaje que desea consultar: ")
+    character_id = int(character_id_input.strip())
+    character = services.get_character_by_id(character_id)
+    print("\nPersonaje:")
     print(character)
+    
+    characters_input = input("\nIngrese los IDs de los personajes separados por comas: ")
+    characters_list = [int(character_id.strip()) for character_id in characters_input.split(",")]
 
-attributes_input = input("\nIngrese los atributos por los que desea filtrar separados por comas: ")
-attributes = [attr.strip() for attr in attributes_input.split(",")]
+    character_group = services.get_multiple_characters(characters_list)
+    print("\nGrupo de personajes:")
+    for character in character_group:
+        print(character)
 
-character_id_input = input("\nIngrese el ID del personaje a filtrar: ")
-character_id = int(character_id_input.strip())
+    attributes_input = input("\nIngrese los atributos por los que desea filtrar separados por comas: ")
+    attributes = [attr.strip() for attr in attributes_input.split(",")]
+    invalid_attributes = []
+    for attribute in attributes:
+        if attribute not in character:
+            invalid_attributes.append(attribute)
+    if invalid_attributes:
+        print(f"\nError: Los siguientes atributos no existen para el personaje consultado: {', '.join(invalid_attributes)}")
+    else:
+        character_id_input = input("\nIngrese el ID del personaje a filtrar: ")
+        character_id = int(character_id_input.strip())
 
-filtered_character = services.filter_character(character_id, attributes)
-if filtered_character:
-    print("\nPersonaje filtrado:")
-    print(filtered_character)
-else:
-    print(f"\nNo se encontró el personaje con ID {character_id}")
+        filtered_character = services.filter_character(character_id, attributes)
+        if filtered_character:
+            print("\nPersonaje filtrado:")
+            print(filtered_character)
+        else:
+            print(f"\nNo se encontró el personaje con ID {character_id}")
+
+except ValueError:
+    print("Error: Ingrese un ID de personaje válido como un número entero.")
